3bdef079a26f5a963b622f062decbddc46d57017
Emily Daly <edaly3@luc.edu>
2021-03-12 16:40:02 -0600

    firebase modules installed

---
+++ index.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDqSvvfp4SSycrWWS5YPaTkk_RZiYd9ZmM",
+    authDomain: "comp-324-story-game.firebaseapp.com",
+    databaseURL: "https://comp-324-story-game-default-rtdb.firebaseio.com",
+    projectId: "comp-324-story-game",
+    storageBucket: "comp-324-story-game.appspot.com",
+    messagingSenderId: "257537359537",
+    appId: "1:257537359537:web:0257937760a7a0268f6c90",
+    measurementId: "G-M8XW1WKDGT"
+  };
