1a3d40162dd1e858130d97b50eca5869f6724aee
Emily Daly <edaly3@luc.edu>
2021-03-18 18:43:38 -0500

    fixed styling issues

--- www/js/script.js
+++ www/js/script.js
@@ -1,11 +1,14 @@
 document.getElementsByTagName('div')[0].innerHTML = 0;
+
 function rob() {
     var z = Math.floor((Math.random() * 7) + 4);
     sessionStorage.setItem("gold", z);
     document.getElementsByTagName('div')[0].innerHTML = z;}
+
 function beg() {
     var y = Math.floor((Math.random() * 5) + 1);
     sessionStorage.setItem("gold", y);
     document.getElementsByTagName('div')[0].innerHTML = y;}
+
 function goldtotal() {document.getElementsByTagName('div')[0].innerHTML = sessionStorage.getItem("gold");}
 
