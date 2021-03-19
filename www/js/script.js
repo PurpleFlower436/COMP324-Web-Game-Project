308a0a18c3f100ba725650973ef4f9828d8990cb
Jason Hajduk <55854661+Ja-Hill-Incar-Nao@users.noreply.github.com>
2021-03-19 03:37:11 -0500

    Update script.js

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,10 @@
-document.getElementsByTagName('div')[0].innerHTML = 0;
-
 function rob() {
     var z = Math.floor((Math.random() * 7) + 4);
-    sessionStorage.setItem("gold", z);
-    document.getElementsByTagName('div')[0].innerHTML = z;}
-
+    sessionStorage.setItem("gold","Gold:" + z);}
 function beg() {
     var y = Math.floor((Math.random() * 5) + 1);
-    sessionStorage.setItem("gold", y);
-    document.getElementsByTagName('div')[0].innerHTML = y;}
-
+    sessionStorage.setItem("gold","Gold:" + y);}
+function session_start() {
+    sessionStorage.setItem("gold","Gold:0");
+    document.getElementsByTagName('div')[0].innerHTML = sessionStorage.getItem("gold");}
 function goldtotal() {document.getElementsByTagName('div')[0].innerHTML = sessionStorage.getItem("gold");}
-
