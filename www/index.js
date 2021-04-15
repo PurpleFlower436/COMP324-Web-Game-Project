9e303994f8b75087dc394c12fc82ce0a19801abf
Emily Daly <edaly3@luc.edu>
2021-04-15 15:41:02 -0500

    firebase code for HTML pages updated, initialization & login code in index.js

--- www/index.js
+++ www/index.js
@@ -1,58 +1,78 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-import "firebase/analytics";
-
 // Add the Firebase products that you want to use
 import "firebase/auth";
-import "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDqSvvfp4SSycrWWS5YPaTkk_RZiYd9ZmM",
-    authDomain: "comp-324-story-game.firebaseapp.com",
-    databaseURL: "https://comp-324-story-game-default-rtdb.firebaseio.com",
-    projectId: "comp-324-story-game",
-    storageBucket: "comp-324-story-game.appspot.com",
-    messagingSenderId: "257537359537",
-    appId: "1:257537359537:web:0257937760a7a0268f6c90",
-    measurementId: "G-M8XW1WKDGT"
-};
-firebase.initializeApp(firebaseConfig);
 
-function loginBtnClick() {
+
+function initFirbase() {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyDqSvvfp4SSycrWWS5YPaTkk_RZiYd9ZmM",
+        authDomain: "comp-324-story-game.firebaseapp.com",
+        databaseURL: "https://comp-324-story-game-default-rtdb.firebaseio.com",
+        projectId: "comp-324-story-game",
+        storageBucket: "comp-324-story-game.appspot.com",
+        messagingSenderId: "257537359537",
+        appId: "1:257537359537:web:0257937760a7a0268f6c90",
+        measurementId: "G-M8XW1WKDGT"
+    };
+    firebase.initializeApp(firebaseConfig);
+}
+
+function login() {
     var email = document.getElementById("Email").value;
     var password = document.getElementById("Password").value;
-    firebase.auth().signInWithEmailAndPassword(email, password).then((userCredetial) => {
-        var user = userCredetial.user;
-        // ...
-    }).catch((error) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredetial) => {
+          var user = userCredetial.user;
+          // ...
+    })
+    .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
+        console.log("The following error has occured: "+errorCode+" / "+ errorMessage);
     });
 }
 
-function signupBtnClick() {
+function signUp() {
     const email = document.getElementById("Email").value;
     const password = document.getElementById("Password").value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
-        var user = userCredential.user;
-        // ...
-    }).catch((error) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+          var user = userCredential.user;
+          // ...
+    })
+    .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
+        console.log("The following error has occured: "+errorCode+" / "+ errorMessage);
     });
 }
 
 function logoutBtnClick() {
     firebase.auth().signOut().then(() => {
-        console.log("Logout Successful!");
+        console.log("Logout Successful");
     }).catch((error) => {
-        console.log("An error has occurred: " + error.message);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log("The following error has occured: "+errorCode+" / "+ errorMessage);
+    });
+}
+
+/* -- Listener for changes in user state -- */
+function authStateListener() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            var uid = user.uid;
+            // ...
+        } else {
+            // User is signed out
+            // ...
+        }
     });
 }
